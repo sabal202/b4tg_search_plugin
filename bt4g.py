b1b7cf326ff446bffd77a8506cf72bef4f693ea2
Sabalevskiy Sergey <sabal2000@mail.ru>
2023-08-06 19:24:56 +0300

    fix dowload torrent

--- bt4g.py
+++ bt4g.py
@@ -1,21 +1,27 @@
-# VERSION: 1.01
-# AUTHORS: kjjejones44
+# VERSION: 1.1
+# AUTHORS: kjjejones44, sabal202
 
 from html.parser import HTMLParser
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlencode
 from helpers import retrieve_url
 from novaprinter import prettyPrinter
 import json
+import re
+
 
 class bt4g(object):
-    
     url = "https://bt4g.org/"
     name = "bt4g"
-    supported_categories = {'all': '', 'movies': 'movie/', 'tv': 'movie/', 'music': 'audio/', 'books': 'doc/', 'software': 'app/'}
-
+    supported_categories = {
+        "all": "",
+        "movies": "movie/",
+        "tv": "movie/",
+        "music": "audio/",
+        "books": "doc/",
+        "software": "app/",
+    }
 
     class MyHTMLParser(HTMLParser):
-
         def __init__(self):
             super().__init__()
             self.is_in_container = False
@@ -29,9 +35,8 @@ class bt4g(object):
             super().feed(feed)
             return self.results
 
-
         def handle_starttag(self, tag, attrs):
-            attr_dict = {x[0]:x[1] for x in attrs}
+            attr_dict = {x[0]: x[1] for x in attrs}
             if tag == "div":
                 if not self.is_in_container and attr_dict.get("class", "") == "container":
                     self.is_in_container = True
@@ -41,11 +46,11 @@ class bt4g(object):
                     self.temp_result.update(attr_dict)
             elif tag == "b":
                 if self.is_in_entry:
-                    classname = attr_dict.get("class", "")                
+                    classname = attr_dict.get("class", "")
                     idname = attr_dict.get("id", "")
                     self.b_value = "filesize" if "cpill" in classname else idname
 
-        def handle_endtag(self, tag):        
+        def handle_endtag(self, tag):
             if tag == "div":
                 self.is_in_entry = False
 
@@ -60,11 +65,11 @@ class bt4g(object):
     def search(self, term, cat="all"):
         pagenumber = 1
         while pagenumber <= 10:
-            result_page = self.search_page(term, pagenumber, cat)            
+            result_page = self.search_page(term, pagenumber, cat)
             self.pretty_print_results(result_page)
-            if len(result_page) < 15 or int(result_page[-1]['seeders']) < 1:
+            if len(result_page) < 15 or int(result_page[-1]["seeders"]) < 1:
                 break
-            pagenumber = pagenumber + 1
+            pagenumber += 1
 
     def search_page(self, term, pagenumber, cat):
         try:
@@ -73,24 +78,33 @@ class bt4g(object):
             return parser.feed(retrieve_url(query))
         except Exception as e:
             return []
-        
+
     def download_torrent(self, info):
         if "trackerlist" not in self.__dict__:
             self.trackerlist = json.loads(retrieve_url("https://downloadtorrentfile.com/trackerlist"))
-        magnet = f"magnet:?xt=urn:btih:{info.split('/')[-1]}&tr=" + "&tr=".join(self.trackerlist)
-        print(magnet + ' ' + info)
+        html = retrieve_url(info)
+        magnet_match = re.search(r'(?<=com/hash/)([^"\?]+)\?name=([^"]*)?(?=")', html)
+        magnet_hash = magnet_dn = None
+        if magnet_match and magnet_match.groups():
+            magnet_hash = magnet_match.groups()[0]
+            magnet_dn = magnet_match.groups()[1]
+            # print(magnet_hash, magnet_dn, info)
+        magnet = f"magnet:?xt=urn:btih:{magnet_hash}&dn={magnet_dn}&{urlencode({'tr': self.trackerlist}, doseq=True)}"
+        print(f"{magnet} {info}")
 
     def pretty_print_results(self, results):
         for result in results:
             temp_result = {
-                'name': result['title'],
-                'size': result['filesize'],
-                'seeds': result['seeders'],
-                'leech': result['leechers'],
-                'engine_url': self.url,
-                'link': urljoin(self.url, result['href'])
+                "name": result["title"],
+                "size": result["filesize"],
+                "seeds": result["seeders"],
+                "leech": result["leechers"],
+                "engine_url": self.url,
+                "link": urljoin(self.url, result["href"]),
             }
             prettyPrinter(temp_result)
 
+
 if __name__ == "__main__":
+    # bt4g().download_torrent("https://bt4g.org/magnet/07f5kZIDw0bvcLUnHv1FwJelosDMMhvWC")
     bt4g().search("how", cat="books")
